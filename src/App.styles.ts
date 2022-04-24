b49dbcc98c7a90f45c3c26570c45e48f63827975
Moabe <moabeluis2018@gmail.com>
2022-04-24 13:23:24 -0300

    Header Criado e estilizado

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -11,3 +11,11 @@ export const Area = styled.div`
     max-width: 980px;
     padding: 10px;
 `
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #FFF;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () =>  {
   return(
     <C.Container>
       <C.Area>
-        ...
+        <C.Header>Lista de Tarefas</C.Header>
       </C.Area>
     </C.Container>
   )
