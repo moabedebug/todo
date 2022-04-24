b182ee8967e9982451f2a2e0ffd91adc2ec473fd
Moabe <moabeluis2018@gmail.com>
2022-04-24 13:32:17 -0300

    Fazendo a tipagem dos itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import * as C from './App.styles'
+import { Item } from './types/item'
 
 const App = () =>  {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão', done: false},
+    { id: 2, name: 'Comprar um bolo', done: false},
+  ])
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área de adicionar nova tarefa */}
+
+        {/* {list.map((item, index)=>(
+          <div>...</div>
+        ))} */}
+
       </C.Area>
     </C.Container>
   )
